0c6eb9c1874585b4998493cbf75a5e95417fd476
jj <jj@jj.com>
2018-11-25 23:21:58 -0500

    working version

--- src/routes/router.js
+++ src/routes/router.js
@@ -7,7 +7,6 @@ var path = require('path');
 var _path = path.join(__dirname + '/../' + '/views/');
 console.log(_path);
 
-//Sessions.find({}, (err, data) => {
 
 router.get('/', (req, res) => {
       if (!req.session.userId) {
@@ -86,7 +85,7 @@ router.post('/login', (req, res, next) => {
            display = {
              email: user.email,
            }
-           return res.render(_path + "index.pug", display)
+           return res.render(_path + "index.pug", display);
         }
     });
   }
@@ -105,22 +104,17 @@ router.get('/logout', (req, res, next) => {
   }
 });
 
-module.exports = router;
-/*
-app.get('/', (req, res) => {
-  res.sendFile(_path + "index.html");
+router.get('/workspace', (req, res, next) => {
+  Sessions.find({}, (err, data) => {
+    if(err) {
+      return(next(err));
+    } else {
+      display = {
+        logged_in:data.length
+      };
+      res.render(_path + "workspace.pug", display);
+    }
+  });
 });
-app.get('/account', (req, res) => {
-  res.sendFile(_path + "account.html");
-});
-app.get('/signup', (req, res) => {
-  res.sendFile(_path + "signup.html");
-});
-app.get('/about', (req, res) => {
-  res.sendFile(_path + "about.html");
-});
-app.post('/signup', (req, res) => {
 
-  res.sendFile(_path + "index.html");
-});
-*/
+module.exports = router;
